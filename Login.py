6093444bc4d8125292bfd87c0f47f0ed4af3370b
Natan <natantavares4982@gmail.com>
2025-05-19 21:24:13 -0300

    Adiciona comentários explicativos em todas as funções

--- Login.py
+++ Login.py
@@ -1,28 +1,45 @@
+# Função para login do usuário
+def login(valores, arquivo): 
+    global logado  # Usuário logado
+
+    # Solicita os dados de login ao usuário
+    userName = input("Digite o nome do usuário: ")
+    userPassword = input("Digite a senha do usuário: ")
+
+    # Percorre a lista de usuários salvos para verificar o login
+    for i in range(len(valores)):
+        if valores[i][0] == userName:
+            if valores[i][1] == userPassword:
+                print("Login bem-sucedido!")
+                logado = i  # Armazena o índice do usuário logado
+                return logado
+            else:
+                print("Senha incorreta.")
+                return None
+
+    print("Usuário não encontrado.")
+    return None
 
 
-def login(valores, arquivo):
-    global logado
-
-    with open(arquivo, 'r') as f:
-        userName = input("Digite o nome do usuário: ")
-        userPassword = input("Digite o nome do usuário: ")
-        
-        for i in range(len(valores)):
-            if valores[i][0] == userName:
-                if valores[i][1] == userPassword:
-                    print("entrou")
-                    logado = i
-                else:
-                    print("senha incorreta")
-
+# Função para registrar um novo usuário
 def registrando(listaDeCadastro, valores, arquivo):
-    global cadastrado
+    global cadastrado  # Usuário cadastrado
 
-    listaDeCadastro.append(input("Digite o nome do usuário: "))
-    listaDeCadastro.append(input("Digite a senha do usuário: "))
+    # Solicita os dados de cadastro ao usuário
+    nome = input("Digite o nome do novo usuário: ")
+    senha = input("Digite a senha do novo usuário: ")
 
+    # Adiciona os dados à lista temporária de cadastro
+    listaDeCadastro.append(nome)
+    listaDeCadastro.append(senha)
+
+    # Adiciona a nova conta à lista completa de valores
     valores.append(listaDeCadastro)
+
+    # Salva os dados atualizados no arquivo
     with open(arquivo, 'w') as f:
         f.write(str(valores))
 
-    cadastrado = len(valores)-1
+    # Armazena o índice do novo usuário cadastrado
+    cadastrado = len(valores) - 1
+    return cadastrado
--- Route.py
+++ Route.py
@@ -1,36 +1,47 @@
+# Função para exibir os treinos de forma formatada na tela
+def treinoFormatado(valores, formatacao): 
+    # Verifica se o usuário tem apenas a entrada de dados cadastrais
+    if len(valores[formatacao]) == 1:
+        print("Nenhum treino cadastrado.\n")
+    
+    # Percorre todos os treinos do usuário
+    for i in range(len(valores[formatacao])):
+        if i > 1:
+            treino = valores[formatacao][i]
+
+            # Exibe a data do treino
+            print(f"{treino[0]}/{treino[1]}/{treino[2]}")
+
+            # Exibe o tipo do treino
+            print("Tipo de treino:", end=" ")
+            if treino[3] == 1:
+                print("AMRAP")
+            elif treino[3] == 2:
+                print("EMOM")
+            elif treino[3] == 3:
+                print("for time")
+            else:
+                print("resposta inválida")
+
+            # Exibe a duração do treino
+            print(f"tempo: {treino[4]} minutos")
+            print("Movimentos:")
+
+            # Exibe os movimentos realizados no treino
+            for j in range(len(treino)):
+                if j >= 5:
+                    print(treino[j])
+            print()  # Linha em branco após cada treino
 
 
-def treinoFormatado(valores,formatacao):
-        if len(valores[formatacao]) == 1:
-            print("Nenhum treino cadastrado.\n")
-        
-        for i in range(len(valores[formatacao])):
-            if i > 1:
-                print(f"{valores[formatacao][i][0]}/{valores[formatacao][i][1]}/{valores[formatacao][i][2]}")#data
-                print("Tipo de treino:", end = " ")#tipo de treino
-
-                if valores[formatacao][i][3] == 1:
-                    print("AMRAP")
-
-                elif valores[formatacao][i][3] == 2:
-                    print("EMOM")
-
-                elif valores[formatacao][i][3] == 3:
-                    print("for time")
-
-                else:
-                    print("resposta inválida")
-                print(f"tempo: {valores[formatacao][i][4]}\nMovimentos:")# duração do treino
-                for j in range(len(valores[formatacao][i])):#movimentos do treino
-                    if j >=5:
-                        print(valores[formatacao][i][j])
-                print()
-
+# Função para carregar os dados salvos no arquivo
 def load(arquivo):
     with open(arquivo, 'r') as f:
-        read = f.read()
+        read = f.read()  # Lê todo o conteúdo do arquivo como string
     return read
 
+
+# Função para sobrescrever o conteúdo do arquivo com novos dados
 def cadastro(valorInputado, arquivo):
     with open(arquivo, 'w') as f:
-        f.write(valorInputado)
+        f.write(valorInputado)  # Escreve os dados no arquivo, substituindo o anterior
